d3f0ce518483dd05c5144496ae273493bf248916
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2026-07-01 14:26:13 +0530

    useing an own aksvd

--- dict_learners/aksvd.py
+++ dict_learners/aksvd.py
@@ -1,10 +1,10 @@
 from dict_learners.dict_learner import DictLearner
-from ksvd import ApproximateKSVD
+from dict_learners.ksvd import ApproximateKSVD
 
 class AKSVD(DictLearner):
     def __init__(
             self,
-            dimensions: int = 32,
+            dimensions: int = 16,
             max_iter: int = 10,
             tol: float = 1e-6,
             n_non_zero_coefs: int = 10,
