3e04e81345c3897b26bac7323983b82335b8fc6e
sparsegraph23-ops <sparsegraph23@gmail.com>
2026-08-02 12:47:16 +0530

    Implemented the wl+bayesian related files

--- utils/registry.py
+++ utils/registry.py
@@ -54,6 +54,10 @@ def _dict_learner_loaders():
         from dict_learners.frozen_ksvd_learner import FrozenKSVDLearner
         return FrozenKSVDLearner
 
+    def _bayesian():
+        from dict_learners.bayesian import BayesianDL
+        return BayesianDL
+
     def _pca_then_dict_learner():
         # Comparison arm only (pca/): a PCA reduction composed with a real
         # dictionary learner, presented to the pipeline as one DictLearner.
@@ -65,6 +69,7 @@ def _dict_learner_loaders():
         "AKSVD": _aksvd,
         "LCKSVDLearner": _lcksvd,
         "FrozenKSVDLearner": _frozen_ksvd,
+        "BayesianDL": _bayesian,
         "PCAThenDictLearner": _pca_then_dict_learner,
     }
 
